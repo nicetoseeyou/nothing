905a5980f66456a274dc024124f46ebd1df69d89
nicetoseeyou <hello4world@yeah.net>
2018-05-22 01:01:09 +0800

    init

---
+++ src/main/java/lab/nice/nothing/App.java
@@ -0,0 +1,13 @@
+package lab.nice.nothing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, Java" );
+    }
+}
---
+++ src/main/scala/lab/nice/nothing/Application.scala
@@ -0,0 +1,7 @@
+package lab.nice.nothing
+
+object Application {
+  def main(args: Array[String]): Unit = {
+    println("Hello, Scala")
+  }
+}
