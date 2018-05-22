150d281bd5aab667bf1429a1861fe71f329f4e89
nicetoseeyou <hello4world@yeah.net>
2018-05-22 21:38:11 +0800

    add kafka, schema registry, log dependency

--- src/main/java/lab/nice/nothing/App.java
+++ src/main/java/lab/nice/nothing/App.java
@@ -1,13 +1,18 @@
 package lab.nice.nothing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
+        LOGGER.info("Hello World");
         System.out.println( "Hello, Java" );
     }
 }
